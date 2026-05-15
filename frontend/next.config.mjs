bc4dfa51f5e4275df3a0a6bcdd1346f0f65e13b4
ayudash <adrianyudhaswara@gmail.com>
2026-05-16 00:03:51 +0700

    update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const isNgrok = process.env.NEXT_PUBLIC_USE_NGROK === 'true';
-const ngrokUrl = process.env.NEXT_PUBLIC_NGROK_API_URL ? new URL(process.env.NEXT_PUBLIC_NGROK_API_URL).hostname : '';
+const ngrokBackendUrl = process.env.NEXT_PUBLIC_NGROK_API_URL ? new URL(process.env.NEXT_PUBLIC_NGROK_API_URL).hostname : '';
+const ngrokFrontendUrl = process.env.NEXT_PUBLIC_NGROK_FRONTEND_URL ? new URL(process.env.NEXT_PUBLIC_NGROK_FRONTEND_URL).hostname : '';
 
 const nextConfig = {
-  ...(isNgrok && ngrokUrl && {
-    allowedDevOrigins: [ngrokUrl]
+  ...(isNgrok && {
+    allowedDevOrigins: [ngrokBackendUrl, ngrokFrontendUrl].filter(Boolean)
   })
 };
 
